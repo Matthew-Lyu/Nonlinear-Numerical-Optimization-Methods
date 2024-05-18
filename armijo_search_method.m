747f50f0f4381dc79b535ae15970673c300744a2
Matthew-Lyu <lyuwch03@gmail.com>
2024-05-19 00:24:28 +0800

    update

--- armijo_search_method.m
+++ armijo_search_method.m
@@ -1,16 +1,31 @@
-function [mk, alpha, fk, newfk] = armijo_search(xk, dk, fun, gfun)
-    rho = 0.1;
-    sigma = 0.2;
-    m = 0;
-    mmax = 20;
+function [mk, alpha, fk, newfk] = armijo_search_method(xk, dk, fun, grad)
+    % Armijo准则搜索算法函数
+    % 输入：
+    %   xk: 当前点
+    %   dk: 搜索方向
+    %   fun: 目标函数
+    %   grad: 梯度函数
+    % 输出：
+    %   mk: 步长指数
+    %   alpha: 步长因子
+    %   fk: 当前点的函数值
+    %   newfk: 新点的函数值
+
+    rho = 0.1; % 步长缩放因子
+    sigma = 0.2; % Armijo条件系数
+    m = 0; % 步长指数
+    mmax = 20; % 最大步长指数
+
     while (m <= mmax)
-        if(fun(xk + rho^m*dk) <= fun(xk) + sigma*rho^m*gfun(xk)'*dk)
-            mk = m; break;
+        if fun(xk + rho^m * dk) <= fun(xk) + sigma * rho^m * grad(xk)' * dk
+            mk = m; % 更新步长指数
+            break;
         end
-        m = m+1;
+        m = m + 1;
     end
-    alpha = rho^mk;
-    newxk = xk + alpha*dk;
-    fk = fun(xk);
-    newfk = fun(newxk);
+
+    alpha = rho^mk; % 计算步长因子
+    newxk = xk + alpha * dk; % 计算新点
+    fk = fun(xk); % 当前点的函数值
+    newfk = fun(newxk); % 新点的函数值
 end
